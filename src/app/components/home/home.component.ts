d9a238707e84cc6c52c69d2728e5d414a9d4953f
ASUS <wajaatef@gmail.com>
2018-10-14 23:32:52 +0200

    test commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Currency} from '../../models/currency.model';
 import {QueryParams} from '../../models/query-params.model';
+import {ObservableMedia} from '@angular/flex-layout';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
+import {CurrenciesService} from '../../services/currencies.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +13,9 @@ import {QueryParams} from '../../models/query-params.model';
 })
 export class HomeComponent implements OnInit {
 
+
+  @ViewChild('grid') grid: MatGridList;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   currencies: Currency[];
   gridByBreakpoint = {
     xl: 6,
@@ -27,9 +34,54 @@ export class HomeComponent implements OnInit {
     {value: 'name', viewValue: 'Name'},
     {value: 'type', viewValue: 'Type'}
   ];
-  constructor() { }
+  constructor(private currenciesService: CurrenciesService, private snackbar: MatSnackBar, private router: Router,
+              private observableMedia: ObservableMedia, private searchService: SearchService) { }
 
   ngOnInit() {
+    this.initQueryParams();
+  }
+  public showDetails(id: string) {
+    this.searchService.params = this.queryParams;
+    this.router.navigate(['/currency/' + id]);
+  }
+
+  ngAfterContentInit() {
+    this.observableMedia.asObservable().subscribe((change: MediaChange) => {
+      this.grid.cols = this.gridByBreakpoint[change.mqAlias];
+    });
+  }
+
+  private initQueryParams() {
+
+    if(this.searchService.params === undefined)
+    {
+      this.queryParams = new QueryParams();
+      this.queryParams.pageIndex = 1;
+      this.queryParams.pageSize = 10;
+      this.queryParams.filterKey = 'any';
+
+    } else {
+      this.queryParams = this.searchService.params;
+    }
+    this.initCurrencies();
+  }
+
+  private initCurrencies() {
+    this.currenciesService.findall(this.queryParams).subscribe(_currenciesPage => {
+      this.currencies = _currenciesPage.currencies;
+      this.currenciesNumber = _currenciesPage.total;
+    });
+  }
+
+  attributeChanged(key) {
+    this.queryParams.filterKey = key;
+    if (this.queryParams.filterValue !== undefined && this.queryParams.filterValue.length > 0) {
+      this.initCurrencies();
+    }
+  }
+  keyChanged(value) {
+    this.queryParams.filterValue = value;
+    this.initCurrencies();
   }
   pageChanged() {
     this.queryParams.pageIndex = this.paginator.pageIndex + 1;
